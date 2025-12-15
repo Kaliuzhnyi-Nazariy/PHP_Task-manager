c95490db6f693ad4636f1f1408ed1786b8a7d14c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-12-15 16:09:21 +0100

    changed to postgres db

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM php:8.2-apache
 
-RUN docker-php-ext-install mysqli
+RUN docker-php-ext-install RUN docker-php-ext-install pdo pdo_pgsql
 
 RUN a2enmod rewrite
 
--- public/connectDB.php
+++ public/connectDB.php
@@ -1,11 +1,14 @@
 <?php
-$DB_HOST = getenv('DB_HOST');
-$DB_USER = getenv('DB_USER');
-$DB_PASSWORD = getenv('DB_PASSWORD');
-$DB_DATABASE = getenv('DB_USER');
+// $DB_HOST = getenv('DB_HOST');
+// $DB_USER = getenv('DB_USER');
+// $DB_PASSWORD = getenv('DB_PASSWORD');
+// $DB_DATABASE = getenv('DB_DATABASE');
+
+$dsn = "pgsql:host=$DB_HOST;port=5432;dbname=$DB_DATABASE";
 
 try {
-    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
+    $conn = new PDO($dsn, $DB_USER, $DB_PASSWORD);
+    // $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
 } catch (\Exception $e) {
     echo 'Error connecting db: ' . $e->getMessage();
 }
