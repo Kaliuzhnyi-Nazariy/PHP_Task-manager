8b0e924eb73e03443ae63e969da036a69c11dabd
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-12-15 10:15:53 +0100

    connect db

---
+++ connectDB.php
@@ -0,0 +1,13 @@
+<?php
+$DB_HOST = getenv('DB_HOST');
+$DB_USER = getenv('DB_USER');
+$DB_PASSWORD = getenv('DB_PASSWORD');
+$DB_DATABASE = getenv('DB_DATABASE');
+
+echo $DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE;
+
+try {
+    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
+} catch (\Exception $e) {
+    echo 'Error connecting db: ' . $e->getMessage();
+}
