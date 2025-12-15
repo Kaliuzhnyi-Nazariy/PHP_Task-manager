1e12e9c169c6ffac52e20c9a759a9b8dcb0e23f1
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-12-15 10:46:52 +0100

    move all files to one folder

--- public/connectDB.php
+++ public/connectDB.php
@@ -2,8 +2,7 @@
 $DB_HOST = getenv('DB_HOST');
 $DB_USER = getenv('DB_USER');
 $DB_PASSWORD = getenv('DB_PASSWORD');
-$DB_DATABASE = getenv('DB_DATABASE');
-
+$DB_DATABASE = getenv('DB_USER');
 
 try {
     $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE);
